b8de2430815acc16e7ec81390e756d1b14745895
lebelokamogelo <lebelokamogelo47@gmail.com>
2024-06-16 20:39:14 +0200

    parse json data

--- request/src/main.rs
+++ request/src/main.rs
@@ -1,8 +1,28 @@
 use reqwest;
+use serde::Deserialize;
+
+// parse the json into struct rust redable format
+#[derive(Deserialize, Debug)]
+struct Post {
+    userId: i32,
+    id: i32,
+    title: String,
+    body: String,
+}
 
 fn main() {
-    let response = reqwest::blocking::get("https://jsonplaceholder.typicode.com/users").unwrap();
+    // blocking client
+    let response = reqwest::blocking::get("https://jsonplaceholder.typicode.com/posts").unwrap();
 
-    let users = response.text();
-    print!("{:?}", users)
+    // matching status code
+    match response.status() {
+        reqwest::StatusCode::OK => {
+            // process data
+            let posts: Vec<Post> = serde_json::from_str(&response.text().unwrap()).unwrap();
+            println!("{:?}", posts[0].title)
+        }
+
+        reqwest::StatusCode::UNAUTHORIZED => println!("You are not authenticated"),
+        _ => print!("Uh oh! Something went wrong"),
+    }
 }
